b315c628f503a80f74b2fcb50deecf4500b089d8
anyewon <ayw000@naver.com>
2023-11-10 14:01:58 +0900

    useCallback_1110

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import CreateUser from './userCreate/CreateUser';
 import UserList from './userCreate/UserList';
-import React, {useState, useRef, useMemo} from "react";
+import React, {useState, useRef, useMemo,useCallback} from "react";
+
+
+// useCallback을 사용하여 특정함수를 새로 만들지 않고 재사용하고 싶을 때 사용하게 한다.
+// 근데 문제는 input이 바뀔 때도 UserList 컴포넌트가 리렌더링이 되고 있음
 
 
 function countAtiveUser(users){
@@ -17,13 +21,15 @@ function App() {
   });
 
   const {username, email} = inputs;
-  const onChange = e=>{
+  const onChange = useCallback(
+    e=>{
       const {name, value} = e.target;
       setInputs({
           ...inputs,
           [name]:value
       });
-  };
+    },[inputs]
+  );
 
   const [users, setUsers]=useState([
       {
@@ -48,7 +54,7 @@ function App() {
   const nextId = useRef(4);
 
   //배열 추가
-  const onCreate = ()=>{
+  const onCreate = useCallback( ()=>{
       const user = {
           id:nextId.current,
           username,
@@ -62,19 +68,24 @@ function App() {
       });
 
       nextId.current+=1;
-  };
+    },[users,username,email]
+  );
   // 배열 삭제
-  const onRemove = id=>{
-    setUsers(users.filter(user=> user.id !== id));
-  }
+  const onRemove = useCallback(
+        id=>{
+        setUsers(users.filter(user=> user.id !== id));
+    },[users]
+  );
   // 배열 수정
-  const onToggle = id => {
-    setUsers(
-        users.map(user=> 
-            user.id===id ? {...user, active: !user.active}:user
-        )
-    );
-  };
+  const onToggle = useCallback(
+        id => {
+        setUsers(
+            users.map(user=> 
+                user.id===id ? {...user, active: !user.active}:user
+            )
+        );
+    },[users]
+  );
 
   const count = useMemo(()=>countAtiveUser(users), [users]);
   return(
